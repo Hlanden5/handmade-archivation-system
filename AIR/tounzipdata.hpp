3d2c83cfff724610ca1b828a72f7237a7a1b016e
Hlanden5 <dyhov.egor@gmail.com>
2022-10-14 17:44:26 +0300

    minor update

--- AIR/tounzipdata.hpp
+++ AIR/tounzipdata.hpp
@@ -1,7 +1,16 @@
 #pragma once
 
+#include <QByteArray>
+#include <QString>
+#include <fstream>
+#include "structs.hpp"
+
 class toUnzipData
 {
+  QString pathOfZip;
+  std::vector<QString> pathOfFiles;
+  std::ofstream fileZip;
+  structs *headerArray;
 public:
     toUnzipData();
 };
