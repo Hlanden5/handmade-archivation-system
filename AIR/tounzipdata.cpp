482527144ea315b8e5078fdd272986f1833336fc
AshenFoxxx <115688517+AshenFoxxx@users.noreply.github.com>
2022-12-18 13:05:11 +0300

    Add files via upload

--- AIR/tounzipdata.cpp
+++ AIR/tounzipdata.cpp
@@ -1,6 +1,23 @@
+#include "crc.hpp"
+#include "structs.hpp"
 #include "tounzipdata.hpp"
+#include <cstddef>
+#include <iostream>
+#include <string>
 
 toUnzipData::toUnzipData()
 {
 
 }
+
+toUnzipData::~toUnzipData()
+{
+}
+size_t toUnzipData::findEOCD(std::string &signature){
+    size_t tmp;
+    //std::string str;
+    tmp = signature.find_last_of(eocd.eocd_signature);
+}
+
+//struct endOfCentralDirectory eocd;
+
