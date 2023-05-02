0114181061f26ecbedfb4ef554ad31b26464ff02
AshenFoxxx <115688517+AshenFoxxx@users.noreply.github.com>
2023-05-02 22:33:15 +0300

    Add files via upload

--- AIR/userinterface.cpp
+++ AIR/userinterface.cpp
@@ -1,5 +1,7 @@
 #include "userinterface.hpp"
 #include "ui_userinterface.h"
+#include "clocale"
+#include "QFileDialog"
 
 userInterface::userInterface(QWidget *parent)
   : QMainWindow(parent)
@@ -11,5 +13,50 @@ userInterface::userInterface(QWidget *parent)
 userInterface::~userInterface()
 {
   delete ui;
+}
+
+
+
+
+void userInterface::on_language_clicked()
+{
+   setlocale(LC_ALL,"rus");
+
+    if (Lan){
+        ui->language->setText(RLanguage);
+        ui->mono_arch->setText(RMono);
+        ui->poly_arch->setText(RPoly);
+        ui->unarch->setText(RUn);
+        ui->input->setText(RIn);
+        ui->output->setText(ROut);
+        ui->label->setText(RLab);
+        Lan=false;
+    }
+    else
+    {
+        ui->language->setText(ELanguage);
+        ui->mono_arch->setText(EMono);
+        ui->poly_arch->setText(EPoly);
+        ui->unarch->setText(EUn);
+        ui->input->setText(EIn);
+        ui->output->setText(EOut);
+        ui->label->setText(ELab);
+        Lan=true;
+    }
+
+}
+
+
+
+
+void userInterface::on_input_clicked()
+{
+    QString input_file = QFileDialog::getOpenFileName(0,QObject::tr("Укажите файл"),QDir::homePath(), QObject::tr("Все файлы (*.*)"));
+}
+
+
+void userInterface::on_output_clicked()
+{
+    QString output_file = QFileDialog::getOpenFileName(0,QObject::tr("Укажите файл"),QDir::homePath(), QObject::tr("Все файлы (*.*)"));
 }
 
--- AIR/userinterface.hpp
+++ AIR/userinterface.hpp
@@ -14,6 +14,30 @@ class userInterface : public QMainWindow
 public:
   userInterface(QWidget *parent = nullptr);
   ~userInterface();
+    QString RLab = "Выберите ваши Файлы";
+    QString RIn = "Ввод";
+    QString ROut = "Вывод";
+    QString RPoly= "Множественная архивация";
+    QString RMono= "Архивация";
+    QString RLanguage = "Русский";
+    QString RUn = "Разархивация";
+    QString ELab = "Choose your Files";
+    QString EIn = "Input";
+    QString EOut = "Output";
+    QString EPoly = "Multiple archiving";
+    QString EMono = "Archiving";
+    QString ELanguage = "English";
+    QString EUn = "Unarchiving";
+    bool Lan = true;
+
+private slots:
+
+  void on_language_clicked();
+
+
+  void on_input_clicked();
+
+  void on_output_clicked();
 
 private:
   Ui::userInterface *ui;
