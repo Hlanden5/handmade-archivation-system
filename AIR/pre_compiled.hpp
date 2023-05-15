206380907feddd132383698aa5f843b03124e347
Hlanden5 <91215833+Hlanden5@users.noreply.github.com>
2023-05-15 23:45:31 +0300

    Add files via upload
    zip.dataFromMulZip(); need only pathOfFiles

---
+++ AIR/pre_compiled.hpp
@@ -0,0 +1,17 @@
+#ifdef __cplusplus
+#include <QByteArray>
+#include <QString>
+#include <QtEndian>
+#include <bitset>
+#include <ctime>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <regex>
+#include <windows.h>
+#include <QIODevice>
+#include <filesystem>
+#include <thread>
+#include <mutex>
+#include <QTextCodec>
+#endif
